96cf9fda4219bc25acf837de83c97a3f23f16871
Priyanshuvts <priyanshuvatz@gmail.com>
2026-08-13 20:30:29 +0530

    Time: 6 ms (7.96%), Space: 71.4 MB (5%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,21 +2,24 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int ans = 0 ;
-        vector<int> store1(n , 1);
-        vector<int> store2(n , 1);
+        int curr = 1 , right = 1;
+        vector<int> left(n , 1);
         for(int i= 1; i < n; i ++){
             if(ratings[i] > ratings[i - 1]) {
-                store1[i] = store1[i - 1] + 1;
+                left[i] = left[i - 1] + 1;
             }
         }
+        int ans = max(left[n - 1] , curr);
         for(int i = n - 2; i >= 0; i --){
             if(ratings[i] > ratings[i + 1]){
-                store2[i] = store2[i + 1] + 1;
+                curr = right + 1;
+                right += 1;
             }
-        }
-        for(int i = 0; i < n; i ++){
-            ans += max(store1[i] , store2[i]);
+            else {
+                curr = 1;
+                right = 1;
+            }
+            ans += max(left[i] , curr);
         }
         return ans;
     }
